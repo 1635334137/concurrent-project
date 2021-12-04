7cf37cfe810553b362e9610b4f4f8b8c3cce1728
1635334137 <18877888233@163.com>
2021-12-05 01:14:41 +0800

    init

---
+++ src/test/java/com/lqj/concurrentproject/ConcurrentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lqj.concurrentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
